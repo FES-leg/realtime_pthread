f27f3aa5b893c32699dabd7856e4075206725977
Esmacat Master S V2 <stefano.dallagasperina@polimi.it>
2021-02-19 18:39:45 +0100

    Added histogram for both RT and nRT threads

--- main.cpp
+++ main.cpp
@@ -7,30 +7,27 @@
 #include <signal.h>             // to catch ctrl-c
 #include <unistd.h>
 
-
+#include <plog/Log.h>
+#include <plog/Appenders/ColorConsoleAppender.h>
 #include "time_spec_operation.h"
 #include "loop_time_stats.h"
 
 #define DEFAULT_LOOP_TIME_NS 1000000L
+#define DEFAULT_APP_DURATION_COUNTS 10000
 #define NSEC_PER_SEC 1000000000L
 #define ALLOWED_LOOPTIME_OVERFLOW_NS 200000L
 
+// Text Color Identifiers
+const string boldred_key = "\033[1;31m";
+const string red_key = "\033[31m";
+const string boldpurple_key = "\033[1;35m";
+const string yellow_key = "\033[33m";
+const string blue_key = "\033[36m";
+const string green_key = "\033[32m";
+const string color_key = "\033[0m";
+
 
 using namespace std;
-
-void timespec_diff(struct timespec *start, struct timespec *stop,
-                   struct timespec *result)
-{
-    if ((stop->tv_nsec - start->tv_nsec) < 0) {
-        result->tv_sec = stop->tv_sec - start->tv_sec - 1;
-        result->tv_nsec = stop->tv_nsec - start->tv_nsec + 1000000000;
-    } else {
-        result->tv_sec = stop->tv_sec - start->tv_sec;
-        result->tv_nsec = stop->tv_nsec - start->tv_nsec;
-    }
-
-    return;
-}
 
 void my_task(){
     cout << "Rehamove" << endl;
@@ -52,7 +49,7 @@ void* rt(void* cookie){
 
 
 
-    while(loop_count<60000){
+    while(loop_count<DEFAULT_APP_DURATION_COUNTS){
 
         realtime_loop_time_stats.loop_starting_point();
 
@@ -61,25 +58,23 @@ void* rt(void* cookie){
         t_period.tv_nsec = DEFAULT_LOOP_TIME_NS;
         TIMESPEC_INCREMENT ( t_next, t_period );
 
-//        clock_gettime ( CLOCK_MONOTONIC, &t_now);
+        //        clock_gettime ( CLOCK_MONOTONIC, &t_now);
 
         if(loop_count%1000==0){
-
-        cout << "Hi, my name is Real-Time! ";
-        timespec_sub(&t_result,&t_now,&t_start);
-        cout << (double) (timespec_to_nsec(&t_result)/1e9) << endl;
-        my_task();
+            timespec_sub(&t_result,&t_now,&t_start);
+            PLOGI << green_key << "RT Clock: " << (double) (timespec_to_nsec(&t_result)/1e9) << color_key << endl;
+            my_task();
         }
         loop_count++;
-        /* Sleep until the next execution*/
 
+        /* Sleep until the next execution*/
         clock_nanosleep ( CLOCK_MONOTONIC, TIMER_ABSTIME, &t_next, nullptr );
         clock_gettime ( CLOCK_MONOTONIC, &t_now);
 
         t_overflow = (t_now.tv_sec*1e9 + t_now.tv_nsec) - (t_next.tv_sec*1e9 + t_next.tv_nsec);
         if(t_overflow > ALLOWED_LOOPTIME_OVERFLOW_NS)
         {
-            cout << "Cycle time exceeded the desired cycle time. Overflowed time: " << t_overflow << endl;
+            cout << red_key << "RT Overflow: " << t_overflow << color_key << endl;
         }
 
     }
@@ -88,17 +83,44 @@ void* rt(void* cookie){
 }
 
 void *nrt(void * federica){
-/* NRT code */
-    struct timespec t_start, t_now;
-    clock_gettime(CLOCK_MONOTONIC, &t_start);
+    /* NRT code */
+    loop_time_stats non_realtime_loop_time_stats("non_realtime_loop_time_stats.txt",loop_time_stats::output_mode::screenout_only);
+    static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender; // Create the 2nd appender.
 
-    while(1)
+    plog::init(plog::info,&consoleAppender);
+
+    struct timespec t_start, t_now, t_next, t_period, t_prev,t_result;
+    clock_gettime(CLOCK_REALTIME, &t_start);
+    t_next = t_start;
+    unsigned long int loop_count = 0;
+    unsigned long int t_overflow = 0;
+
+    /* Calculate the time for the execution of this task*/
+    t_period.tv_sec = 0;
+    t_period.tv_nsec = DEFAULT_LOOP_TIME_NS;
+
+    while(loop_count<DEFAULT_APP_DURATION_COUNTS*2)
     {
-        clock_gettime ( CLOCK_MONOTONIC, &t_now);
-    cout << "Hello World non Real-Time! ";
-    cout << (double) (t_now.tv_nsec/1e9+(t_now.tv_sec-t_start.tv_sec)) << endl;
-    usleep(1000000);
+        t_prev = t_next;
+        non_realtime_loop_time_stats.loop_starting_point();
+        if(loop_count%1000==0){
+            timespec_sub(&t_result,&t_now,&t_start);
+            PLOGI << yellow_key << "NRT Clock: " << (double) (timespec_to_nsec(&t_result)/1e9) << color_key << endl;
+        }
+        loop_count++;
+        timespec_add_nsec(&t_next, &t_prev, DEFAULT_LOOP_TIME_NS);
+        clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &t_next, NULL);
+        clock_gettime ( CLOCK_REALTIME, &t_now);
+
+        t_overflow = (t_now.tv_sec*1e9 + t_now.tv_nsec) - (t_next.tv_sec*1e9 + t_next.tv_nsec);
+        if(t_overflow > ALLOWED_LOOPTIME_OVERFLOW_NS)
+        {
+            cout << red_key << "NRT Overflow: " << t_overflow << color_key << endl;
+        }
     }
+
+    non_realtime_loop_time_stats.store_loop_time_stats();
+
 }
 
 int main()
@@ -119,7 +141,7 @@ int main()
     pthread_attr_setschedparam(&attr,&prio);
 
     if ( pthread_create(&rt_loop, &attr, rt, nullptr ) ){
-        cout <<  "Error: rt not created" ;
+        PLOGE <<  "Error: rt not created" ;
         return 1;
     }
 
@@ -133,7 +155,7 @@ int main()
     pthread_attr_setschedparam(&attr,&prio);
 
     if ( pthread_create(&nrt_loop, &attr, nrt, nullptr) ){
-        cout << "Error: nrt not created" ;
+        PLOGE << "Error: nrt not created" ;
         return 1;
     }
 
